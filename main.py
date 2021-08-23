7661e63d03ebbe80195c9af3d026c1728c030d99
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-23 16:17:21 -0300

    Encerramos a aula de pré processamento.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv(r'04_dados_aula.csv')
 
@@ -28,6 +30,23 @@ features = np.array(columnTransformer.fit_transform(features))
 
 labelEncoder = LabelEncoder()
 classe = labelEncoder.fit_transform(classe)
-print (classe)
 
 
+features_treinamento, features_teste, classe_treinamento, classe_teste = train_test_split(
+    features, classe, test_size=0.2, random_state=1
+)
+
+# print (features_treinamento)
+# print ('----------------')
+# print (features_teste)
+# print ('----------------')
+# print (classe_treinamento)
+# print ('----------------')
+# print (classe_teste)
+print (features_treinamento)
+standardScaler = StandardScaler()
+features_treinamento[:, 3: ] = standardScaler.fit_transform(features_treinamento[ : , 3: ])
+print (features_treinamento)
+
+features_teste[:, 3: ] = standardScaler.transform(features_teste[:, 3:])
+print (features_teste)
